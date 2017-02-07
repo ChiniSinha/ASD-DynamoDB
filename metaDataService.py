950c02d8b007fc10c921d5b0839d83789162456e
Chini Sinha <chinisinha@gmail.com>
2017-02-06 20:15:31 -0500

    Chini: Retrieving data from dynamo db

---
+++ metaDataService.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import boto3
+import json
+
+username = 'Chini'
+client = boto3.client('dynamodb')
+response = client.get_item( TableName='MetaData', Key={'UserName': { 'S': username }})
+print response
